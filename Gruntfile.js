936ec973eb03bc76f29f475bf434b947f363891c
anzizhao <anzizhao@126.com>
2016-10-09 11:52:15 +0800

    功能 测试打包脚本添加

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,8 +3,41 @@ module.exports  = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         run: {
-            json: {
-                 exec: 'npm run json',
+            testTag: {
+                 cmd: 'git',
+                 args: [
+                     'tag',
+                     '-a',
+                     grunt.option("tag"),
+                     '-m',
+                     grunt.option("message"),
+                 ]
+            },
+            build: {
+                exec: 'npm run build',
+            },
+            delTagVersion: {
+                exec: 'rm -f tag_version_*',
+            },
+            addTagVersion: {
+                 cmd: 'touch',
+                 args: [
+                     'tag_version_' + grunt.option("tag"),
+                 ]
+            },
+            testTar: {
+                cmd: 'tar',
+                args: [
+                    '-zcf',
+                     'tag_' + grunt.option("tag") + '_.tar.gz',
+                     './tag_version_' + grunt.option("tag"),
+                     './ares.html',
+                     './article.html',
+                     './js/dist/',
+                     './js/other/',
+                     './css/',
+                     './img/'
+                ]
             }
         },
         sass: {
@@ -41,5 +74,19 @@ module.exports  = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch'); 
     grunt.loadNpmTasks('grunt-contrib-sass'); 
 
+    //打包测试版本
+    //添加tag   编译  打包成果物
+    grunt.registerTask('packTest', "grunt packTest --tag=  --message=  ",
+                       function(){
+                           var cmds = [   
+                               'run:testTag',
+                               'run:build',
+                               'run:delTagVersion',
+                               'run:addTagVersion',
+                               'run:testTar',
+                           ];
+                           grunt.task.run( cmds )
+                       }
+                      );
 };
 
